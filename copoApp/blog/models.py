da6cd7527635dc0d0420df23448dd8a8b6c7070e
Shlomo Gelman <shlomog@mobileye.com>
2024-02-14 10:51:36 +0200

    models change, adding fields and Comment\Tag class

--- copoApp/blog/models.py
+++ copoApp/blog/models.py
@@ -2,13 +2,35 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Post(models.Model):
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
     title = models.CharField(max_length=255)
     img_url = models.URLField()
     group_url = models.URLField()
     description = models.TextField()
     likes = models.IntegerField(default=0)
     saved_by = models.ManyToManyField(User, related_name="saved_posts")
+    date_published = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name="products", blank=True)
+    original_price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    product = models.ForeignKey(
+        Product, related_name="comments", on_delete=models.CASCADE
+    )
+    comment = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.date} on {self.product.title}"
--- copoApp/blog/views.py
+++ copoApp/blog/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Product
 
 
 class HomeView(ListView):
-    model = Post
+    model = Product
     template_name = "home.html"
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "product_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product = self.get_object()
+        comments = product.comments.all()  # Fetch comments associated with the product
+        context["comments"] = comments
+        return context
