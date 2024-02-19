74a1b6267b3aace8f9e9b585f3324b4cabdbd3f3
Shlomo Gelman <shlomog@mobileye.com>
2024-02-19 16:19:17 +0200

    adding product detail view

--- copoApp/blog/urls.py
+++ copoApp/blog/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, ProductDetailView
 
-urlpatterns = [path("", HomeView.as_view(), name="home")]
+urlpatterns = [
+    path("", HomeView.as_view(), name="home"),
+    path("product/<int:pk>", ProductDetailView.as_view(), name="product-detail"),
+]
